0bfc2fa8cc354a4d9b200ab6174c1bec4af7dbe3
Aline Souza <alines@ciandt.com>
2025-06-08 18:25:42 -0300

    adding cors config

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors' // <-- importe o CORS
 import { connectDB } from './config/db.js';
 import servicoRoutes from './routes/servico.route.js'
 import agendamentoRoutes from './routes/agendamento.route.js'
@@ -12,6 +13,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api/servicos', servicoRoutes);
@@ -23,4 +26,4 @@ app.use('/api/solicitarServicos', solicitarservicoRoutes);
 app.listen(PORT, () => {
     connectDB();
     console.log("Server started at http://localhost:" + PORT)
-})
+});
